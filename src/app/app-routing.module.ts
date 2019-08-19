1f6d32a48b4d1b5f0e3f626eadb1e5bd7ea6c57c
Bruno Fernandes <bruno.fernandes@airc.pt>
2019-08-19 10:08:43 +0100

    base, login and dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BaseComponent } from './base/base.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', component : LoginComponent},
+  {path : 'login', component : LoginComponent},
+  {path: '', component: BaseComponent,
+    children: [
+      {path : 'dashboard', component : DashboardComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { BaseComponent } from './base/base.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './footer/footer.component';
+import { TopbarComponent } from './topbar/topbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    BaseComponent,
+    SidebarComponent,
+    DashboardComponent,
+    FooterComponent,
+    TopbarComponent
   ],
   imports: [
     BrowserModule,
